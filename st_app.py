976ed871f807cebc43ab3cd0db51610c46ccce68
Michael Kalish <michael.kalish@berkeley.edu>
2022-10-13 20:54:44 -0700

    First iteration of file done

--- st_app.py
+++ st_app.py
@@ -2,4 +2,31 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.write('hi')
+st.header('UC Berkeley, MIDS')
+st.subheader('Just an app')
+
+sb = st.sidebar.radio('Select an operation',['*','/','+','-'])
+
+x = st.number_input('Pick a number', 1, 10)
+y = st.number_input('Pick another number', 1, 10)
+z = 0
+
+if sb == '*':
+    z = x * y
+elif sb == '/':
+    numerator = st.sidebar.radio('Which number should be the numerator?',[f'{x}',f'{y}'])
+    if numerator == x:
+        z = x / y
+    else:
+        z = y / x
+elif sb == '+':
+    z = x + y
+elif sb == '-':
+    start_number = st.sidebar.radio('Which number should we subtract from?',[f'{x}',f'{y}'])
+    if start_number == x:
+        z = x - y
+    else:
+        z = y - x
+
+st.balloons(f'The answer to your problem is {z}')
+

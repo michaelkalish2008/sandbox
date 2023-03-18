f7270fa1bbc05ec7309876316fe8dfd67c4bec95
Michael Kalish <michael.kalish@berkeley.edu>
2023-03-17 20:01:32 -0700

    Generates audio file of GDPR

--- text_to_audio.py
+++ text_to_audio.py
@@ -6,26 +6,29 @@ import os
 import re
 
 # STEP 2: Select file and open file and save content to variable
-fpath = './gdpr.txt'
+# fpath = './gdpr_part1_preamble_to_art17.txt'
+# fpath = './gdpr_part2_art18_to_art52.txt'
+fpath = './gdpr_part3_art53_to_end.txt'
 f = open(fpath, 'r')
 content = f.read()
 content = re.sub(r'\s+',' ',content)
 
 # STEP 4: Convert content to audio file and select language
-language = 'en'
-myobj = gTTS(text=content[:200],
-             lang=language,
-             tld='ie',
+myobj = gTTS(text=content,
+             lang='en',
              slow=False)
 
 # STEP 5: Save mp3 to director and close txt file
-myobj.save('gdpr_short.mp3')
+# myobj.save('gdpr_part1_start_to_art17.mp3')
+# myobj.save('gdpr_part2_art18_to_art52.mp3')
+myobj.save('gdpr_part3_art53_to_end.mp3')
 f.close()
 
 # FIN
 
 # NOTES
 # Documentation for gtts: https://gtts.readthedocs.io/en/latest/module.html
-# Using conda? Here you go. conda install -c conda-forge gtts
+# Using conda? conda install gtts
 # Want to compress the file? "Check out Simple Audio Compression With Python"
 # Medium, url 'https://medium.com/@jmpion/simple-audio-compression-with-python-70bdd7535b0a'
+# python3 text_to_audio.py
